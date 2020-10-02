81e58f5ec279d1cbfc845fdeb9b23f38b88e94ad
Yogesh <39991349+yogesh147@users.noreply.github.com>
2020-10-02 12:41:03 +0530

    print cube of number
    this program give cube of number

---
+++ PrintCube.java
@@ -0,0 +1,10 @@
+public class PrintCube{
+
+     public static void main(String []args){
+      int a = 2; 
+       System.out.print(printCube(a));
+     }
+     public static int printCube(int a){
+         return a*a*a;
+     }
+}
